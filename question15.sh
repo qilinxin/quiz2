fd68f425869a2b2b0e5b6f4412a1233a6c934868
qilinxin <qilinxin21@gmail.com>
2024-03-09 21:59:28 +1030

    aa

---
+++ question15.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/ian-knight-uofa/git-practice-01.git
+cd git-practice-01
+git fetch origin branch1
+git pull
+git merge origin/branch1
+cd dir1
+vi file3
+git add file3
+git commit -m 'resolve confict'
