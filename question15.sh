d46c3fc9912bad006f9bc7091e85e3f75fd70234
qilinxin <qilinxin21@gmail.com>
2024-03-10 20:12:22 +1030

    u q156

--- question15.sh
+++ question15.sh
@@ -1,9 +1,6 @@
-git clone https://github.com/ian-knight-uofa/git-practice-01.git
 cd git-practice-01
 git fetch origin branch1
 git pull
 git merge origin/branch1
-cd dir1
-vi file3
-git add file3
+git add .
 git commit -m 'resolve confict'
--- question16.sh
+++ question16.sh
@@ -1,5 +1,5 @@
 cd git-practice-02
 git checkout branch3
-git checkout branch3
+git checkout branch2
 git merge branch3
 git branch -d branch3
